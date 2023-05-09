4b47e5133fe290520df66f75a5d42e249fa17628
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-05-09 19:38:46 +0000

    a medias

--- src/js/component/semaforo.js
+++ src/js/component/semaforo.js
@@ -1,25 +1,20 @@
 import React from "react";
 
 const Semaforo = () => {
+
+    
     return (
-        <div className="container">
-            <div className="row">
-                <div className="palo bg-black mx-auto">
-                    <p></p>
-                </div>
-            </div>
-            <div className="row">
-                <div className="base bg-black mx-auto">
-                    <div className="row mx-auto">
-                        <button className="rojo bg-danger rounded-circle border-0">.</button>
+        <div className="container bg-image">
+            <div className="base bg-black mx-auto">
+                    <div className="row">
+                        <button className="light bg-danger border-0"></button>
                     </div>
                     <div className="row">
-                        <button className="amarillo bg-warning rounded-circle border-0">.</button>
+                        <button className="light bg-warning border-0"></button>
                     </div>
                     <div className="row">
-                        <button className="verde bg-success rounded-circle border-0">.</button>
+                        <button className="light bg-success border-0"></button>
                     </div>
-                </div>
             </div>
         </div>
     );
