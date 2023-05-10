6e95bdb44abf8a40af4cc7677f7cb33cb57f0569
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-05-10 16:17:45 +0000

    a medias

--- src/js/component/semaforo.js
+++ src/js/component/semaforo.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Semaforo = () => {
 
-    
+    const [selectedColor, setSelectedColor ] = useState ("red");
     return (
         <div className="container bg-image">
             <div className="base bg-black mx-auto">
                     <div className="row">
-                        <button className="light bg-danger border-0"></button>
+                        <div onClick={() => selectedColor ("red")} className={"light red" + ((selectedColor === "red") ? "glow" : "" )}></div>
                     </div>
                     <div className="row">
-                        <button className="light bg-warning border-0"></button>
+                        <div onClick={() => selectedColor ("yellow")} className={"light yellow" + ((selectedColor === "yellow") ? "glow" : "" )}></div>
                     </div>
                     <div className="row">
-                        <button className="light bg-success border-0"></button>
+                        <div onClick={() => selectedColor ("green")} className={"light green" + ((selectedColor === "red") ? "glow" : "" )}></div>
                     </div>
             </div>
         </div>
     );
 };
 
-export default Semaforo
+export default Semaforo;
