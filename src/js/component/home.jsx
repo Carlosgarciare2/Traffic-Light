1434539f45312a3e99c6c4dd79d65b3d43c2ce63
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-05-10 17:04:01 +0000

    finish

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Semaforo from "./semaforo";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
--- src/js/component/semaforo.js
+++ src/js/component/semaforo.js
@@ -7,13 +7,22 @@ const Semaforo = () => {
         <div className="container bg-image">
             <div className="base bg-black mx-auto">
                     <div className="row">
-                        <div onClick={() => selectedColor ("red")} className={"light red" + ((selectedColor === "red") ? "glow" : "" )}></div>
+                        <div 
+                        onClick={() => setSelectedColor ("red")} 
+                        className={"light red" + (selectedColor === "red" ? " glow" : "" )
+                        }></div>
                     </div>
                     <div className="row">
-                        <div onClick={() => selectedColor ("yellow")} className={"light yellow" + ((selectedColor === "yellow") ? "glow" : "" )}></div>
+                        <div 
+                        onClick={() => setSelectedColor ("yellow")} 
+                        className={"light yellow" + (selectedColor === "yellow" ? " glow" : "" )
+                        }></div>
                     </div>
                     <div className="row">
-                        <div onClick={() => selectedColor ("green")} className={"light green" + ((selectedColor === "red") ? "glow" : "" )}></div>
+                        <div 
+                        onClick={() => setSelectedColor ("green")} 
+                        className={"light green" + (selectedColor === "green" ? " glow" : "" )
+                        }></div>
                     </div>
             </div>
         </div>
